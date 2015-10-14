6bf7842561c48674ea5124613085d44f63ddf971
sanchez <dartsyms@gmail.com>
2015-10-14 18:46:31 +0400

    Add user model and POST /users request

--- server.js
+++ server.js
@@ -65,6 +65,17 @@ app.post('/todos', function(req, res) {
     });
 });
 
+// POST /users
+app.post('/users', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.create(body).then(function(user) {
+		res.json(user.toJSON());
+	}, function(e) {
+		res.status(400).json(e);
+	});
+});
+
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
