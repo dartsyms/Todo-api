d5799dff1fc8b35cde8f19a844b27fd869f77f66
sanchez <dartsyms@gmail.com>
2015-10-14 19:36:38 +0400

    Hash and salt passwords

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function(user) {
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}, function(e) {
 		res.status(400).json(e);
 	});
@@ -126,7 +126,9 @@ app.put('/todos/:id', function(req, res) {
 
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({
+	// force: true
+}).then(function() {
 	app.listen(PORT, function() {
     	console.log('Express listening on port ' + PORT + '!');
 	});
